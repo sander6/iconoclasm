b394f19a4060a511588b0d8471abdcf1f68cdfd3
Sander Hartlage <sander@kgbweb.com>
2009-12-21 18:14:05 -0500

    Wrote (simple) spec for the main module

--- spec/iconoclast_spec.rb
+++ spec/iconoclast_spec.rb
@@ -2,4 +2,23 @@ require File.expand_path(File.dirname(__FILE__) + '/helper')
 
 describe Iconoclast do
   
+  describe "#extract" do
+    before do
+      @url = 'http://www.website.com/some-crappy-blog-post'
+    end
+    
+    it "should extract the favicon for the given url" do
+      Iconoclast.expects(:extract_favicon_from).with(@url)
+      Iconoclast::Favicon.stubs(:new)
+      Iconoclast.extract(@url)
+    end
+    
+    it "should make a new Favicon instance" do
+      favicon = stub('favicon')
+      Iconoclast.stubs(:extract_favicon_from).returns(favicon)
+      Iconoclast::Favicon.expects(:new).with(favicon)
+      Iconoclast.extract(@url)
+    end
+  end
+  
 end
