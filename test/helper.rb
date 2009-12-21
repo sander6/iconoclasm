df66fb46f112f45456673993192a2f273afce450
Sander Hartlage <sander@kgbweb.com>
2009-12-21 17:44:57 -0500

    Set up RSpec in the test helper instead of Shoulda

--- test/helper.rb
+++ test/helper.rb
@@ -1,10 +1,11 @@
 require 'rubygems'
-require 'test/unit'
-require 'shoulda'
+require 'spec'
+require 'mocha'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'iconoclast'
 
-class Test::Unit::TestCase
+Spec::Runner.configure do |config|
+  config.mock_with :mocha
 end
