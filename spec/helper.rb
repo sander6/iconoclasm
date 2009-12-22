fa4a3b402796b82ddce7fff1c9db8810cdb64737
Sander Hartlage <sander@kgbweb.com>
2009-12-22 18:05:46 -0500

    Patched in some symbol-throwing behavior into mocha

--- spec/helper.rb
+++ spec/helper.rb
@@ -9,3 +9,26 @@ require 'iconoclast'
 Spec::Runner.configure do |config|
   config.mock_with :mocha
 end
+
+# It bothers me that mocha doesn't have this built-in.
+# It also bothers me that I forked mocha but didn't add this.
+# We are all at fault.
+module Mocha
+  class Expectation
+    def throws(symbol, object = nil)
+      @return_values += ReturnValues.new(SymbolThrower.new(symbol, object))
+      self
+    end
+  end
+  
+  class SymbolThrower
+    def initialize(symbol, object)
+      @symbol = symbol
+      @object = object
+    end
+    
+    def evaluate
+      @object ? throw(@symbol, @object) : throw(@symbol)
+    end
+  end
+end
